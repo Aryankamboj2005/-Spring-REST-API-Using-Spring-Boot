5d2dbfcf0b412fe652d2027b20586141d0ecd3e6
Aryan <Aryankamboj2005@gmail.com>
2026-04-05 09:27:13 +0530

    getting data on the console usig the spring boot

--- src/main/java/com/Aryan/SimpleWebApp/controller/productController.java
+++ src/main/java/com/Aryan/SimpleWebApp/controller/productController.java
@@ -1,7 +1,29 @@
 package com.Aryan.SimpleWebApp.controller;
 
+import com.Aryan.SimpleWebApp.Model.product;
+import com.Aryan.SimpleWebApp.service.productService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
 public class productController {
-    public String getproduct(){
-        return " ";
+
+//    @Autowired
+//    productService service;
+//
+//    @GetMapping("/products")
+//    public List<product> getProducts() {
+//        return service.getProduct();
+//    }
+    @Autowired
+    productService service;
+
+    @RequestMapping("/products")
+    public List<product> getProduct() {
+        return service.getProduct();
     }
 }
--- src/main/java/com/Aryan/SimpleWebApp/service/productService.java
+++ src/main/java/com/Aryan/SimpleWebApp/service/productService.java
@@ -1,4 +1,21 @@
 package com.Aryan.SimpleWebApp.service;
 
+import com.Aryan.SimpleWebApp.Model.product;
+import org.springframework.stereotype.Service;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+// make the object of the class so that we can uuse it at any other place
 public class productService {
+
+    List<product> products = Arrays.asList(
+            new product(100, "laptop", 101),
+            new product(50, "mouse", 102),
+            new product(150, "camera", 103)
+    );
+
+    public List<product> getProduct() {
+        return products;
+    }
 }
